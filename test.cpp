9ba92d172927b4ab904a637ead6c610371c5b3d1
shinra1000 <shinra1000@gmail.com>
2021-03-30 20:19:17 +0900

    test

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  cout << "ABCDEFGHIJKLMNOPQRSTUVWXYZ" << endl;
+  cout << "abcdefghijklmnopqrstuvwxyz" // 長いので改行してインデント
+  << endl;
+}
